4e2f70062c6b243751745a449a211f2b22adfb67
Unknown <jordon.bowden@gmail.com>
2018-05-11 08:35:28 -0600

    Changed up nav bar a bit

--- scripts/advertiseScript.js
+++ scripts/advertiseScript.js
@@ -13,22 +13,35 @@ function loadComplete(evt) {
   data = JSON.parse(request.responseText);
   for(var i = 0; i < data.length; i++) {
     var div = document.createElement("div");
-    div.className = "proj-box";
+    div.className = "proj-box row";
+    var div1 = document.createElement("div");
+    div1.className = "proj-picture flex-1";
     var img = document.createElement("img");
     img.src = data[i].image;
+    var div2 = document.createElement("div");
+    div2.className = "proj-info flex-2 txt-center";
     var name = document.createElement("a");
     name.innerHTML = data[i].title;
     name.href = data[i].link;
+    name.className = "proj-name";
     var need = document.createElement("p");
     need.innerHTML = data[i].need;
-    need.className = "bold";
+    need.className = "proj-need bold";
     var desc = document.createElement("p");
     desc.innerHTML = data[i].description;
-    desc.className = "cutoff";
-    div.appendChild(img);
-    div.appendChild(name);
-    div.appendChild(need);    
-    div.appendChild(desc);
+    desc.className = "proj-desc cutoff";
+    div2.appendChild(name);
+    div2.appendChild(need);    
+    div2.appendChild(desc);
+    div1.appendChild(img);
+    if(i % 2 == 1) {
+      div.appendChild(div1);
+      div.appendChild(div2);
+    }
+    else {
+      div.appendChild(div2);
+      div.appendChild(div1);
+    }
     currentProjects.appendChild(div);    
   }
 }
